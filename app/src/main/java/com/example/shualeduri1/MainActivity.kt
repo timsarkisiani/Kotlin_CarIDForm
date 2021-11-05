56ef691b1445abc558d99569dbadc0877a0ddf4d
timsarkisiani <92742586+timsarkisiani@users.noreply.github.com>
2021-11-05 17:59:26 +0400

    განახლება

--- app/src/main/java/com/example/shualeduri1/MainActivity.kt
+++ app/src/main/java/com/example/shualeduri1/MainActivity.kt
@@ -8,17 +8,73 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var gagzavna: Button
-    private lateinit var ki1: Button
-    private lateinit var ara1: Button
-    private lateinit var ki2: Button
-    private lateinit var ara2: Button
+    private lateinit var ki1: CheckBox
+    private lateinit var ara1: CheckBox
+    private lateinit var ki2: CheckBox
+    private lateinit var ara2: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         gagzavna = findViewById(R.id.gagzavna)
-        gagzavna.setOnClickListener() {
-            Toast.makeText(applicationContext, "წარმატებით გაიგზავნა", Toast.LENGTH_SHORT).show()
+        ki1 = findViewById(R.id.ki1)
+        ara1 = findViewById(R.id.ara1)
+        ki2 = findViewById(R.id.ki2)
+        ara2 = findViewById(R.id.ara2)
+
+        fun uncheck(){
+            if (ki1.isChecked){
+                ki1.toggle()
+            }
+            if (ara1.isChecked){
+                ara1.toggle()
+            }
+            if (ki2.isChecked){
+                ki2.toggle()
+            }
+            if (ara2.isChecked){
+                ara2.toggle()
+            }
+        }
+
+        fun check() {
+            ki1.setOnClickListener() {
+                if (ara1.isChecked){ara1.toggle()}
+            }
+            ara1.setOnClickListener() {
+                if (ki1.isChecked){ki1.toggle()}
+            }
+
+            ki2.setOnClickListener() {
+                if (ara2.isChecked){ara2.toggle()}
+            }
+            ara2.setOnClickListener() {
+                if (ki2.isChecked){ki2.toggle()}
+            }
+
+        }
+        check()
+        gagzavna.setOnClickListener(){
+            if(ki1.isChecked){
+                Toast.makeText(applicationContext,"წარმატებით გაიგზავნა",Toast.LENGTH_SHORT).show()
+                uncheck()
+            }
+
+            if(ara1.isChecked){
+                Toast.makeText(applicationContext,"წარმატებით გაიგზავნა",Toast.LENGTH_SHORT).show()
+                uncheck()
+            }
+            if(ki2.isChecked){
+                Toast.makeText(applicationContext,"წარმატებით გაიგზავნა",Toast.LENGTH_SHORT).show()
+                uncheck()
+            }
+            if(ara2.isChecked){
+                Toast.makeText(applicationContext,"წარმატებით გაიგზავნა",Toast.LENGTH_SHORT).show()
+                uncheck()
+            }
         }
     }
 }
